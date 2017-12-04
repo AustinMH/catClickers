74d28f136f18eead644c1f4db1437bc1de096367
AustinMH <austinmunschhayhurst@gmail.com>
2017-12-04 11:47:52 -0600

    clickedCat undefined bug

--- premiumMVO/app.js
+++ premiumMVO/app.js
@@ -1,7 +1,7 @@
 //Model
+var clickedCat = "";
 
 var model = {
-    clickedCat: null,
     cats: [
         {
             clickCount: 0,
@@ -35,7 +35,9 @@ var model = {
 
 var controller = {
     init: function () {
-        console.log("controller.init();!");
+        console.log("controller.init();");
+        clickedCat = model.cats[0];
+        console.log(clickedCat);
         this.render();
     },
 
@@ -47,9 +49,13 @@ var controller = {
     getAllCats: function () {
         return model.cats;
     },
+
+    assignClickedCat: function(catCopy) {
+        clickedCat = catCopy;
+    },
     
     incrementCounter: function () {
-        
+        console.log("increment counter");
     }
 };
 
@@ -75,16 +81,44 @@ var populateList = {
             cat = allCats[i];
             li = document.createElement("li");
             li.textContent = cat.name;
+            li.id = cat.name;
             ul.appendChild(li);
             console.log(li);
-        }  
+
+            li.addEventListener('click', (function(catCopy) {
+                return function() {
+                    controller.assignClickedCat(catCopy);
+                    console.log(catCopy);
+                    createCatEntry.init();
+                };
+            })(cat));
+            
+        }
     }
 };
 
+
 var createCatEntry = {
+    init: function () {
+        this.name = document.getElementById("name");
+        this.img = document.getElementById("img");
+        this.count = document.getElementById("count");
+        this.clickTarget = document.getElementById("clickTarget");
+        this.clickTarget = document.getElementById("clickTarget");
+        
+        this.clickTarget.addEventListener('click', function(){
+            controller.incrementCounter();
+        });
+        
+        this.render();
+    },
     
-    //Define Which Cat to c
-    
+    render: function() {
+        var clickedCat = controller.assignClickedCat();
+        this.name.textContent = clickedCat.name;
+        this.img.src = clickedCat.img;
+        this.count.textContent = clickedCat.clickCount;
+    }
 };
 
 //Start
